4fe68eefd20b8ed368adec12dabfb2f8a2dea397
opilane <opilane@HP-CZC2349HT3>
2023-03-17 09:51:55 +0200

    Add project files.

---
+++ Horoskoop_Ajaplaan/Horoskoop.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Horoskoop_Ajaplaan
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Horoskoop : ContentPage
+    {
+        DatePicker dp;
+        Label lbl;
+        public Horoskoop()
+        {
+            lbl = new Label
+            {
+                Text = "Vali mingi kuupäev",
+                BackgroundColor = Color.Turquoise
+            };
+            dp = new DatePicker
+            {
+                Format = "D",
+                MinimumDate = DateTime.Now.AddDays(-5),
+                MaximumDate = DateTime.Now.AddDays(5),
+                TextColor = Color.BlueViolet,
+            };
+            dp.DateSelected += Dp_DateSelected;
+            AbsoluteLayout abs = new AbsoluteLayout
+            {
+                Children = { lbl, dp }
+            };
+            AbsoluteLayout.SetLayoutBounds(lbl, new Rectangle(0.1, 0.2, 200, 50));
+            AbsoluteLayout.SetLayoutFlags(lbl, AbsoluteLayoutFlags.PositionProportional);
+            AbsoluteLayout.SetLayoutBounds(lbl, new Rectangle(0.1, 0.5, 300, 50));
+            AbsoluteLayout.SetLayoutFlags(lbl, AbsoluteLayoutFlags.PositionProportional);
+            AbsoluteLayout.SetLayoutBounds(lbl, new Rectangle(0.5, 0.7, 300, 50));
+            AbsoluteLayout.SetLayoutFlags(lbl, AbsoluteLayoutFlags.PositionProportional);
+            Content = abs;
+        }
+
+        private void Dp_DateSelected(object sender, DateChangedEventArgs e)
+        {
+            lbl.Text = "Oli valitud kuupäev: " + e.NewDate.ToString("G");
+        }
+    }
+}
---
+++ Horoskoop_Ajaplaan/StartPage.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Horoskoop_Ajaplaan
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class StartPage : ContentPage
+    {
+        List<ContentPage> contentPage = new List<ContentPage>() { new Horoskoop()};
+        List<string> tekstid = new List<string> { "Horoskoop"};
+        public StartPage()
+        {
+            StackLayout st = new StackLayout
+            {
+                Orientation = StackOrientation.Vertical,
+                BackgroundColor = Color.Beige,
+            };
+            for (int i = 0; i < contentPage.Count; i++)
+            {
+                Button button = new Button
+                {
+                    Text = tekstid[i],
+                    TabIndex = i,
+                    BackgroundColor = Color.MediumSpringGreen,
+                    TextColor = Color.Black,
+                };
+                button.Clicked += Navig_funktsion;
+                st.Children.Add(button);
+            }
+
+            Content = st;
+        }
+        private async void Navig_funktsion(object sender, EventArgs e)
+        {
+            Button b = (Button)sender;
+            await Navigation.PushAsync(contentPage[b.TabIndex]);
+        }
+    }
+}
